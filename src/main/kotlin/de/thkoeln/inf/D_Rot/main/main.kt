0a36469ee1de07e5faf79f167cea04609c07414e
SpookyGhost <wr3r3w6j3boa6h@mailbox.org>
2019-10-18 13:37:48 +0200

    push to master xD

--- src/main/kotlin/de/thkoeln/inf/D_Rot/main/main.kt
+++ src/main/kotlin/de/thkoeln/inf/D_Rot/main/main.kt
@@ -5,3 +5,21 @@ package de.thkoeln.inf.D_Rot.main
 fun main(){
     println("helloWorld")
 }
+
+/**
+ * Graph class handles all nodes
+ */
+class Graph{
+    val nodes: ArrayList<ArrayList<Int?>> = arrayListOf()
+    fun addNode(dependencies: ArrayList<Int> ){
+        val l = arrayListOf<Int>()
+        dependencies.forEach {
+            l.add(it,-1)
+            nodes[it][nodes.lastIndex+1]
+
+        }
+
+    }
+}
+
+
